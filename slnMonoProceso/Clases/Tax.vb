6a9744fdccdba01a6d564ec889f2061a7d17463e
andres lopez <ralopezcastano51@gmail.com>
2020-10-29 23:19:58 -0500

    Limpieza de proyecto - 20201029

--- slnMonoProceso/Clases/Tax.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Tax
-    Public Property Porcentaje As Integer
-    Public Property Valor As Decimal
-    Public Property Descipcion As String
-    Public Property id As String
-End Class
